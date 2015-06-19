81a8a80acdcea97c709437bb780fdf646343c9e1
octodoger <octodoger@octodoger.oct>
2015-06-20 01:03:26 +0300

    Create bla.js

---
+++ level1/level2/bla.js
@@ -0,0 +1,20 @@
+// Hello.
+//
+// ------
+//
+
+function main() {
+  return "Hello, World!";
+}
+
+function main2() {
+  return "Hello, World!"
+}
+
+
+function main3 () {
+  if (bla. == null) return;
+  return "Hello, World!"
+}
+
+main();
