255d62fdb8c70d8c0dd1b9f62859d18e712a679e
dimhold <dimhold@gmail.com>
2015-06-20 02:08:48 +0300

    A lot of diff in one file

--- level1/level2/bla.js
+++ level1/level2/bla.js
@@ -7,16 +7,36 @@ function main() {
   return "Hello, World!";
 }
 
-function main2() {
+function main5() {
   return "Hello, World!"
 }
 
 function main2() {
+  return "Hello, World!"
+}
+
+function main22() {
+  return "Hello, World!"
+}
+
+function main() {
+  return "Hello, World!";
+}
+
+function main() {
+  return "Hello, World!";
+}
+
+function main() {
+  return "Hello, World!";
+}
+
+function main33() {
   return "Hello, World!"
 }
 
 function main3 () {
-  if (bla. == null) return;
+  if (bla == null) return;
   return "Hello, World!"
 }
 
